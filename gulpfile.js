3fca99f06b9cb56b695f493d6197d6d9148538d7
Angel Ricardo Matos Martinez <angelrmatoz@gmail.com>
2025-01-22 12:59:21 -0400

    Proyecto Property Group

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const terser = require('gulp-terser');
+const concat = require('gulp-concat');
+const fs = require('fs');
+const browserSync = require('browser-sync').create();
+const webp = require('gulp-webp');
+
+// Inicializar Browser Sync
+function browserSyncInit(cb) {
+    browserSync.init({
+        server: {
+            baseDir: ["./src/pages", "./src", "./"],
+            routes: {
+                "/dist": "dist",
+                "/node_modules": "node_modules"
+            }
+        },
+        port: 3000,
+        notify: false
+    });
+    cb();
+}
+
+// Crear directorios si no existen
+function createDirs(cb) {
+    // Crear estructura de carpetas
+    ['src/js', 'dist/css', 'dist/js', 'dist/img'].forEach(dir => {
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+    });
+    cb();
+}
+
+// Compilar SCSS a CSS
+function css() {
+    return gulp
+        .src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+}
+
+// Compilar JavaScript
+function javascript() {
+    return gulp
+        .src([
+            'src/js/utils/**/*.js',  // Primero todos los archivos de utilidades
+            'src/js/main.js'         // Después el archivo principal
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.min.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+}
+
+// Convertir imágenes a WebP
+function versionWebp() {
+    return gulp
+        .src('src/assets/images/**/*.{png,jpg,jpeg}')
+        .pipe(webp())
+        .pipe(gulp.dest('dist/img'))
+        .pipe(browserSync.stream());
+}
+
+// Crear archivo JavaScript inicial si no existe
+function createInitialJS(cb) {
+    const initialJSPath = 'src/js/main.js';
+    if (!fs.existsSync(initialJSPath)) {
+        fs.writeFileSync(initialJSPath, '// Archivo JavaScript principal');
+    }
+    cb();
+}
+
+// Vigilar cambios en archivos
+function watchFiles() {
+    gulp.watch('src/scss/**/*.scss', css);
+    gulp.watch('src/js/**/*.js', javascript);
+    gulp.watch('src/assets/images/**/*.{png,jpg,jpeg}', versionWebp);
+    gulp.watch('src/pages/**/*.html').on('change', browserSync.reload);
+}
+
+// Tareas
+exports.css = css;
+exports.js = javascript;
+exports.webp = versionWebp;
+exports.watch = watchFiles;
+exports.build = gulp.parallel(css, javascript, versionWebp);
+exports.default = gulp.series(
+    createDirs,
+    createInitialJS,
+    gulp.parallel(css, javascript, versionWebp),
+    browserSyncInit,
+    watchFiles
+); 
---
+++ src/js/utils/updateYear.js
@@ -0,0 +1,9 @@
+// Función para actualizar el año
+function updateYear() {
+    const yearElement = document.getElementById('year');
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+}
+
+updateYear();
